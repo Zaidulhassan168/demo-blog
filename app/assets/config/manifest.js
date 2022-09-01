188d4dff4e228f3c9b768885e397df084fa8ef92
Zaidulhassan168 <zaidulhassan@elgrocer.com>
2022-09-01 20:04:58 +0500

    precompile

--- app/assets/config/manifest.js
+++ app/assets/config/manifest.js
@@ -1,6 +1,6 @@
 //= link_tree ../images
-//= link_directory ../stylesheets.css
-//= link_tree ../../javascript.js
-//= link_tree ../../../vendor/javascript.js
+//= link_directory ../stylesheets .css
+//= link_tree ../../javascript .js
+//= link_tree ../../../vendor/javascript .js
 //= link graphiql/rails/application.css
 //= link graphiql/rails/application.js
