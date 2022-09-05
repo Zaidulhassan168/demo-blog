d3e02f3a5fa3d1890e48af7109d6960f9a480d4e
Zaidulhassan168 <zaidulhassan@elgrocer.com>
2022-09-05 11:10:30 +0500

    sigin error fixed

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -3,19 +3,19 @@
 class Users::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
-  GET /resource/sign_in
+  GET '/resource/sign_in'
   def new
     console
     super
   end
 
-  POST /resource/sign_in
+  POST '/resource/sign_in'
   def create
     console
     super
   end
 
-  DELETE /resource/sign_out
+  DELETE '/resource/sign_out'
   def destroy
     console
     super
