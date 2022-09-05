2e2d046563cb180d365b15aa5fbb9fb95ee2e5fe
Zaidulhassan168 <zaidulhassan@elgrocer.com>
2022-09-05 15:44:18 +0500

    commit

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -2,6 +2,7 @@ require 'redis'
 REDIS = Redis.new(url: ENV.fetch('REDIS_URL') { 'redis://localhost:6379/' })
 
 $redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+Redis.current = Redis.new(:host => '127.0.0.1', :port => 6379)
 
 # if Rails.env.development?
 #     puts "dev: #{ENV['REDIS_URL']}"
