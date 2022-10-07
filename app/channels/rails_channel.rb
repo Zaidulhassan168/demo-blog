98eec93a09df374ab47662efe2f98c8dec471fcf
Zaidulhassan168 <zaidulhassan@elgrocer.com>
2022-10-07 16:42:05 +0500

    added more methods

--- app/channels/rails_channel.rb
+++ app/channels/rails_channel.rb
@@ -3,16 +3,33 @@ class RailsChannel < ApplicationCable::Channel
     
     stream_for "rails_channel"
     # transmit(posts) # send message to current connection (sender)
+    # ActionCable.server.broadcast("rails_channel")
   end
 
-  def speak
+  def getAllProducts
     # num = args.to_json
-    result = Post.limit(1)
+    result = Post.limit(10)
     # result = posts.
-    stream_for "rails_channel"
+    # stream_for "rails_channel"
+    transmit(result) # send message to current connection (sender)
+    # puts "send this data.#{params}"
+    ActionCable.server.broadcast("rails_channel", result)
+  end
+  def getSpecificProduct(params)
+    # num = args.to_json
+     puts "id is #{params['args']}"
+    result = Post.find(params['args'])
+    # result = posts.
+    # stream_for "rails_channel"
     transmit(result) # send message to current connection (sender)
     # puts "send this data.#{params}"
     # ActionCable.server.broadcast("rails_channel", "send this data #{params}")
+  end
+
+  def getRating (params)
+    result = Post.find(params['args'])
+    obj = { "total_rating": result.total_rating, "rating_count": result.rating_count, "avg_rating": result.avg_rating , "title": result.title}
+    transmit(obj)
   end
 
   def unsubscribed
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_cable.mount_path = '/wssocket'
-  # config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, /file:\/\/*/, 'file://', nil]
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_cable.allowed_request_origins = [/http:\/\/*/, /https:\/\/*/, /file:\/\/*/, 'file://', 'file:///\/\/*/', nil, /.*/]
   
 end
